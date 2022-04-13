c4cf9a1d944dbb8c12b765a301eebb4b60f09544
13574 <1357496606@qq.com>
2022-04-13 16:02:56 +0800

    feat: Files can be downloaded from Workshop.

--- bin/wsm.js
+++ bin/wsm.js
@@ -5,8 +5,29 @@ const path = require("path");
 
 const packageJSON = require("../package.json");
 const { Command } = require("commander");
+const { downloadFromWorkshop } = require("../lib/workshop")
 
 const program = new Command();
+
+
+async function extractFile(archive, dest, encoding) {
+  if (path.extname(archive) !== ".wsm") {
+    throw new Error("It is not a wsm file.");
+  }
+
+  const file = fs.readFileSync(archive);
+  const WsmInstance = await loadLive2dWsmAsync(file.buffer, encoding);
+
+  dest = path.resolve(dest || process.cwd(), path.basename(archive, ".wsm"));
+
+  for (const [_, { name, data }] of WsmInstance.files) {
+    fs.outputFile(path.resolve(dest, name), new Uint8Array(data), (err) => {
+      if (err) throw err;
+
+      console.log(path.resolve(dest, name));
+    });
+  }
+};
 
 program
   .name(packageJSON.name)
@@ -44,6 +65,7 @@ program
           const dirname = path.basename(filename, ".wsm");
 
           const file = fs.readFileSync(path.resolve(cwd, filename));
+
           const WsmInstance = await loadLive2dWsmAsync(file.buffer);
 
           console.log(path.resolve(cwd, dirname));
@@ -69,23 +91,31 @@ program
     "identifies the encoding. Default: 'utf-8'. See: https://nodejs.org/api/util.html#class-utiltextdecoder"
   )
   .description("extract archive")
-  .action(async (archive, dest, encoding) => {
-    if (path.extname(archive) !== ".wsm") {
-      throw new Error("It is not a wsm file.");
+  .action(extractFile);
+
+program
+  .command("download")
+  .alias("d")
+  .description("download the .wsm file from Workshop and extract it")
+  .argument("<url>", "url to live2d file in Workshop")
+  .argument("[output]", "path to store and output directory")
+  .argument(
+    "[encoding]",
+    "identifies the encoding. Default: 'utf-8'. See: https://nodejs.org/api/util.html#class-utiltextdecoder"
+  )
+  .option("-e,--extract", "extract the .wsm file after download it")
+  .option("-d,--delete","delete the .wsm file after extract\nNote: this switch only avaliable when --extract specified")
+  .action(async (url, dest, encoding, options) => {
+    const baseDir = (dest || process.cwd())
+    console.log("Downloading file...")
+    const file =await downloadFromWorkshop(url, baseDir);
+    console.log(`download ${file} succeed`);
+    if (options["extract"]) {
+      extractFile(file, baseDir, encoding)
+      if(options["delete"]){
+        await fs.unlink(file);
+      }
     }
-
-    const file = fs.readFileSync(archive);
-    const WsmInstance = await loadLive2dWsmAsync(file.buffer, encoding);
-
-    dest = path.resolve(dest || process.cwd(), path.basename(archive, ".wsm"));
-
-    for (const [_, { name, data }] of WsmInstance.files) {
-      fs.outputFile(path.resolve(dest, name), new Uint8Array(data), (err) => {
-        if (err) throw err;
-
-        console.log(path.resolve(dest, name));
-      });
-    }
-  });
+  })
 
 program.parse(process.argv);
